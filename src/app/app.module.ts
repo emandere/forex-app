afdccce5e743a8d8e15fdb7119b10f684b0b3bd8
emandere <emandere@gmail.com>
2019-05-02 04:05:43 -0400

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule, MatListModule } from "@angular/material";
+
 
 import { AppComponent } from './app.component';
+import { LiveSessionsComponent } from './live-sessions/live-sessions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LiveSessionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', component: LiveSessionsComponent,pathMatch: 'full' }
+    ]),
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/live-sessions/live-sessions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-live-sessions',
+  templateUrl: './live-sessions.component.html',
+  styleUrls: ['./live-sessions.component.css']
+})
+export class LiveSessionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
